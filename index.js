23da264c3201c019b443b67db455c79a30146785
Alina web2025-5 <shablenkoo@gmail.com>
2025-03-28 17:31:10 +0200

    Додано обробку аргументів командного рядка та веб-сервер

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("web2025-5!");
+const http = require('http');
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+  .requiredOption('-h, --host <host>', 'Адреса сервера')
+  .requiredOption('-p, --port <port>', 'Порт сервера', parseInt)
+  .requiredOption('-c, --cache <path>', 'Шлях до кешу');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server work!\n');
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Сервер запущено на http://${options.host}:${options.port}`);
+});
+
