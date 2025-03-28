ca3ff77c5573cdb89f26eef97064315fdcf28791
Alina web2025-5 <shablenkoo@gmail.com>
2025-03-28 18:33:03 +0200

    Додано підтримку кешування зображень і обробки HTTP запитів

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const { Command } = require('commander');
+const fs = require('fs').promises;
+const path = require('path');
 
 const program = new Command();
 
@@ -9,15 +11,63 @@ program
   .requiredOption('-c, --cache <path>', 'Шлях до кешу');
 
 program.parse(process.argv);
-
 const options = program.opts();
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server work!\n');
+const server = http.createServer(async (req, res) => {
+  const code = req.url.slice(1); 
+  const filePath = path.join(options.cache, `${code}.jpg`);
+
+  if (!/^\d{3}$/.test(code)) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    return res.end('Невірний формат HTTP-коду');
+  }
+
+  switch (req.method) {
+    case 'GET':
+      try {
+        const image = await fs.readFile(filePath);
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(image);
+      } catch (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Картинка не знайдена');
+      }
+      break;
+
+    case 'PUT':
+      let imageData = [];
+      req.on('data', chunk => imageData.push(chunk));
+      req.on('end', async () => {
+        try {
+          await fs.writeFile(filePath, Buffer.concat(imageData));
+          res.writeHead(201, { 'Content-Type': 'text/plain' });
+          res.end('Картинка збережена');
+        } catch (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Помилка запису файлу');
+        }
+      });
+      break;
+
+    case 'DELETE':
+      try {
+        await fs.unlink(filePath);
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Картинка видалена');
+      } catch (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Файл не знайдено');
+      }
+      break;
+
+    default:
+      res.writeHead(405, { 'Content-Type': 'text/plain' });
+      res.end('Метод не дозволений');
+  }
 });
 
 server.listen(options.port, options.host, () => {
   console.log(`Сервер запущено на http://${options.host}:${options.port}`);
 });
+
 
